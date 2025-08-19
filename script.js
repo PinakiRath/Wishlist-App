1c33550fef4ba5fe1cafe66cc320d0cebd291725
Pinaki <pinakirath99@gmail.com>
2025-08-19 19:24:19 +0530

    new

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let input = document.querySelector(".input");
 let button = document.querySelector(".button");
-let todo;
-let todolist = [];
-button.addEventListener("click", (e)=>{
-    e.preventDefault();
-    todo = input.value;
-    if(todo.length > 0){
-        todolist.push(todo)
+let container = document.querySelector(".container");
+
+button.addEventListener("click", function(e) {
+    e.preventDefault(); 
+    let todo = input.value.trim(); 
+
+    if(todo !== "") {
+        let item = document.createElement("div");
+        item.classList.add("wishlist-item");
+        item.innerHTML = `<span>${todo}</span>
+        <span class="material-icons-outlined">delete</span>`;
+        container.appendChild(item);
+        input.value = "";
+
+        item.querySelector(".material-icons-outlined").addEventListener("click", ()=> {
+            item.remove();
+        });
     }
-    console.log("clicked - " + todo);
 });
